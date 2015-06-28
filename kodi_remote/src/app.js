ac016b9a93c673e98476af6407763b25bb5cecbc
jamarzka <jamarzka@gmail.com>
2015-06-27 21:02:35 -0400

    Working with hard-coded hosts

--- kodi_remote/src/app.js
+++ kodi_remote/src/app.js
@@ -1,60 +1,131 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
+var Accel = require('ui/accel');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+Accel.init();
+
+var host = null;
+var playerId = null;
+var checkInterval = null;
+
+var cmdMenuSections = {
+  current: null,
+  inputCmds: {
+    items: [
+      { title: 'Select', cmd: 'Input.Select' },
+      { title: 'Up', cmd: 'Input.Up' },
+      { title: 'Down', cmd: 'Input.Down' },
+      { title: 'Left', cmd: 'Input.Left' },
+      { title: 'Right', cmd: 'Input.Right' },
+      { title: 'Back', cmd: 'Input.Back' },
+      { title: 'Home', cmd: 'Input.Home' },
+      { title: 'Info', cmd: 'Input.Info' },
+      { title: 'Menu', cmd: 'Input.ContextMenu' }
+    ]
+  },
+  playerCmds: {
+    items: [
+      { title: 'Stop', cmd: 'Player.Stop' },
+      { title: 'Play/Pause', cmd: 'Player.PlayPause' },
+      { title: 'Forward 10 min', cmd: 'Player.Seek', params: { value: 'bigforward' } },
+      { title: 'Backward 10 min', cmd: 'Player.Seek', params: { value: 'bigbackward' } },
+      { title: 'Forward 30 sec', cmd: 'Player.Seek', params: { value: 'smallforward' } },
+      { title: 'Backward 30 sec', cmd: 'Player.Seek', params: { value: 'smallbackward' } }
+    ]
+  }
+};
+
+var hostMenu = new UI.Menu({
+  sections: [{
+    title: 'Hosts',
+    items: [
+      { title: 'Downstairs', ip: '10.1.1.116' },
+      { title: 'Upstairs', ip: '10.1.1.115' }
+    ]
+  }]
 });
 
-main.show();
+var cmdMenu = new UI.Menu();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+hostMenu.show();
+
+checkInterval = setInterval(showCmdMenu, 1000);
+
+hostMenu.on('select', function(e) {
+  host = e.item;
+
+  showCmdMenu();
 });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
+function showCmdMenu() {
+  if(host) {
+    send(host, { name: 'GetActivePlayers', cmd: 'Player.GetActivePlayers' }, function(data, status, request) {
+      
+      var current = cmdMenuSections.current;
+      
+      if(data.result.length > 0) {
+        playerId = data.result[0].playerid;
+        cmdMenuSections.current = 'playerCmds';
+      }
+      else {
+        playerId = undefined;
+        cmdMenuSections.current = 'inputCmds';
+      }
+
+      if(current !== cmdMenuSections.current) {
+        cmdMenu._selection = { sectionIndex: 0, itemIndex: 0 };
+        cmdMenu.section(0, cmdMenuSections[cmdMenuSections.current]);
+        cmdMenu.show();
+      }
+    });
+  }
+}
+
+hostMenu.on('show', function(e) {
+  cmdMenuSections.current = null;
+  host = null;
 });
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+cmdMenu.on('select', function(e) {
+  var cmd = e.item;
+
+  cmd.params = cmd.params || {};
+
+  if(cmdMenuSections.current === 'playerCmds') {
+    cmd.params.playerid = playerId;
+  }
+
+  send(host, cmd, showCmdMenu);
 });
+
+cmdMenu.on('accelTap', function(e) {
+  if(cmdMenuSections.current === 'inputCmds') {
+    send(host, { name: 'Select', cmd: 'Input.Select' }, showCmdMenu);  
+  }
+  else if(cmdMenuSections.current === 'playerCmds') {
+    send(host, { name: 'Play/Pause', cmd: 'Player.PlayPause', params: { playerid: playerId } }, showCmdMenu);  
+  }
+});
+
+function send(host, cmd, success) {
+  success = success || function(data, status, request) {};
+
+  ajax(
+    {
+      url: 'http://' + host.ip + '/jsonrpc?' + cmd,
+      method: 'post',
+      type: 'json',
+      data: { "jsonrpc":"2.0", "method": cmd.cmd, "id": 1, params: cmd.params }
+    },
+    success,
+    function(error, status, request) {
+      var errorCard = new UI.Card({
+        title:'Error',
+        body: 'Unable to complete request'
+      });
+
+      errorCard.show();
+    }
+  );
+}
+
